1b631310d919f8b499c53dec6e302e99b7bb33ad
Muzzasaurus <99302729+Muzzasaurus@users.noreply.github.com>
2023-08-09 17:56:13 +1200

    Added a few more jumps and a jump counter

--- jumpScripts.js
+++ jumpScripts.js
@@ -1,22 +1,31 @@
 const jumps = [
 /* 1 Star */["Gate", "Half Diagonal", "Pineapple Jump", "Shuriken", "Sideways Gate", "T-Bone", "Crusher"],
-/* 2 Star */["Cherry Ledge", "Bubble", "16 Pixel", "Diagonal", "Drop Gate", "Half Diamond", "M Jump 2", "M Jump", "Rukito Ledge", "T-Bone Vortex", "Scrotum", "Platform Jump", "Trophy Jump", "Diamond"],
-/* 3 Star */["Buffed Diagonal 2", "Buffed Diagonal", "Corner", "Charlie", "DJump Diamond", "Ceiling Double Diamond", "Double Diamond", "4 5 Block", "Hourglass", "Invert", "Ledge", "Leehe Jump", "M Jump 3", "Nine Jump", "Plane", "Rukito Jump", "Sphincter", "Steak", "Super F", "John Corner"],
-/* 4 Star */["Topi Jump", "Cascade", "DJump Ceiling Diamond", "Double DPlane", "Double Invert", "DPlane", "Hades Ledge", "Hades Plane", "Mini F"],
-/* 5 Star */["Cosowl", "2 5 Floor Invert", "Double Plane", "EX15", "Killer Block F Jump"]
+/* 2 Star */["Cherry Ledge", "Bubble", "16 Pixel", "Diagonal", "Drop Gate", "Half Diamond", "M Jump 2", "M Jump", "Rukito Ledge", "T-Bone Vortex", "Scrotum", "Platform Jump", "Trophy Jump", "Diamond", "Driek Jump 2"],
+/* 3 Star */["Buffed Diagonal 2", "Buffed Diagonal", "Corner", "Charlie", "DJump Diamond", "Ceiling Double Diamond", "Double Diamond", "4 5 Block", "Hourglass", "Invert", "Ledge", "M Jump 3", "Nine Jump", "Plane", "Rukito Jump", "Sphincter", "Steak", "Super F", "John Corner", "Driek Jump"],
+/* 4 Star */["Topi Jump", "Cascade", "DJump Ceiling Diamond", "Double DPlane", "Double Invert", "DPlane", "Hades Ledge", "Hades Plane", "Mini F", "Leehe Jump", "Double Driek Jump"],
+/* 5 Star */["Cosowl", "2 5 Floor Invert", "Double Plane", "EX15", "Killer Block F Jump"],
+/* 6 Star */["Hendeca Corner"]
 ]
 minDifficulty = 0;
 maxDifficulty = 5;
+jumpNum=0;
+totalDifficulty = 0;
 function randomiseJump() {
-	let jumpDifficulty = Math.floor(Math.random()*(maxDifficulty-minDifficulty)+minDifficulty);
+	jumpNum++;
+	if (Math.floor(Math.random()*1000000) == 0) {
+		jumpDifficulty = 5
+	} else {
+		jumpDifficulty = Math.floor(Math.random()*(maxDifficulty-minDifficulty)+minDifficulty);
+	}
+	totalDifficulty += jumpDifficulty+1;
 	let jumpChoice = jumps[jumpDifficulty][Math.floor(Math.random()*jumps[jumpDifficulty].length)];
 	document.getElementById(`jumpImage`).setAttribute(`src`, `Jumps/${jumpChoice}.png`);
 	if (jumpChoice == "4 5 Block") {
-		document.getElementById(`jumpName`).innerHTML = `Jump: 4.5 Block`;
+		document.getElementById(`jumpName`).innerHTML = `Jump ${jumpNum}: 4.5 Block`;
 	} else if (jumpChoice == "2 5 Floor Invert") {
-		document.getElementById(`jumpName`).innerHTML = `Jump: 2.5 Floor Invert`;
+		document.getElementById(`jumpName`).innerHTML = `Jump ${jumpNum}: 2.5 Floor Invert`;
 	} else {
-		document.getElementById(`jumpName`).innerHTML = `Jump: ${jumpChoice}`;
+		document.getElementById(`jumpName`).innerHTML = `Jump ${jumpNum}: ${jumpChoice}`;
 	}
 	document.getElementById("jumpDifficulty").innerHTML = "";
 	for (let i = 0; i < jumpDifficulty+1; i++) {
@@ -25,6 +34,7 @@ function randomiseJump() {
 	for (let i = 0; i < 4-jumpDifficulty; i++) {
 		document.getElementById("jumpDifficulty").innerHTML += "☆";
 	}
+	console.log("total difficulty: " + totalDifficulty);
 }
 function maxStars(num) {
 	maxDifficulty = num;
